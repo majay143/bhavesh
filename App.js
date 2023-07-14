756684c62ede59446a3560dfb59567f14e4b78f5
mashapogu ajay <mashapoguajay@gmail.com>
2023-07-14 16:38:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import Users from  './components/Users';
+function App() {
+  return (
+    <Users />
+  );
+}
+
+export default App;
+/* const numbers = [1, 2, 3, 4, 5];
+
+const doubledNumbers = numbers.map((number) => number * 2);
+
+console.log(doubledNumbers); example of array.map method */
